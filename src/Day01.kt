da04c9605b993209fdf84686295b9e1a36b1b392
KunalShivnekar <kunal.shivnekar@gmail.com>
2024-12-01 20:16:51 +0530

    Day 01 Part 1 Solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,23 @@
+import java.util.regex.Pattern
+import kotlin.math.absoluteValue
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val list1 = mutableListOf<Int>()
+        val list2 = mutableListOf<Int>()
+        var distance = 0
+        input.forEach { line ->
+            line.split(Pattern.compile("\\s+")).apply {
+                list1.add(get(0).toInt())
+                list2.add(get(1).toInt())
+            }
+        }
+        list1.sort()
+        list2.sort()
+        for (i in input.indices){
+            distance += (list1[i] - list2[i]).absoluteValue
+        }
+        return distance
     }
 
     fun part2(input: List<String>): Int {
@@ -8,11 +25,11 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    //check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 900)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
